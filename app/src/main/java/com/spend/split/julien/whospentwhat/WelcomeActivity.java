214928de1cc490f8ebfb34f5b2d7a0002ef62ad0
Julien <molasjulien@gmail.com>
2016-12-26 19:46:12 +0100

    -- Init project

---
+++ app/src/main/java/com/spend/split/julien/whospentwhat/WelcomeActivity.java
@@ -0,0 +1,13 @@
+package com.spend.split.julien.whospentwhat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+}
